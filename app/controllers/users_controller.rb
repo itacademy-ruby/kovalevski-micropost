4bf90a70c8f8911507d35c63b93c26a22fb7556a
Alexander Randa <randa.alex@active.by>
2013-06-29 12:10:27 +0300

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'Book was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :login
 
   has_many :microposts
+
+  validates :login, :length => {:minimum => 5}
 end

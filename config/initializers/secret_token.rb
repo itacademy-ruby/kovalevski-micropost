647f8000d14bb5f9122b8b4a60173feffad38ff2
Alexander Randa <randa.alex@active.by>
2013-06-29 11:33:41 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RandaalexTwitter::Application.config.secret_token = 'b70e02156e58eef929daa4ec291fe20475185443192bb91b38343b2b0e91020813576a9137c1abe5adcf70133d3cf5d8c01b72711a5167592e8d10b3900de7a0'

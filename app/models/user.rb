1ad813a69ae87867005dab03742a1b84498e2413
Dmitry <dkov@dkov-desktop.(none)>
2013-07-11 15:22:52 +0300

    valid_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
   has_many :microposts
 
-  validates(:email, presence: true)
-  validates :login, :length => {:minimum => 5}
+  validates :login, presence: true, :length => {:minimum => 5, :maximum => 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  
  end
